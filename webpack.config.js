e8575a3abeed66f46c89129e04c2666d8283fa1a
sway <40759948+sway9119@users.noreply.github.com>
2022-11-05 16:32:16 +0900

    webpackの設定

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const {ewsolve, resolve} = require('path')
+
+module.exports= {
+  // mode: どの環境向けにビルドするか（develop, default=production）
+  mode: 'devlopment', 
+  // devtool: ソースマップがあればtypescriptのまま参照できるようにするツール
+  devtool: 'inline-source-map', 
+  // entry: バンドルするファイルのエントリポイント。複数ファイルを指定可能
+  entry: resolve(__dirname, 'ts/index.ts'), 
+  // output: バンドルされてあファイルを出力する場所、出力方法
+  output: { 
+    filename: 'index,js',
+    path: resolve(__dirname, 'dist'),
+  },
+  // resolve: モジュールの解決方法を指定する。拡張しを指定する。
+  resolve: {
+    extension: ['.ts', '.js'],
+  },
+  // module: モジュールとして扱いたいファイルを指定する。それおれloaderを記述する。
+  module: [
+    rule: [
+      {
+        test: /\.ts/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+    ],
+  ]c
+}
