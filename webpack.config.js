100ae45b260a60b810fe6ceca05d920ca3d785e2
sway <40759948+sway9119@users.noreply.github.com>
2022-11-05 16:40:20 +0900

    webpack.config.jsのタイポの修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
-const {ewsolve, resolve} = require('path')
+const { ewsolve, resolve } = require("path");
 
-module.exports= {
+module.exports = {
   // mode: どの環境向けにビルドするか（develop, default=production）
-  mode: 'devlopment', 
+  mode: "development",
   // devtool: ソースマップがあればtypescriptのまま参照できるようにするツール
-  devtool: 'inline-source-map', 
+  devtool: "inline-source-map",
   // entry: バンドルするファイルのエントリポイント。複数ファイルを指定可能
-  entry: resolve(__dirname, 'ts/index.ts'), 
+  entry: resolve(__dirname, "ts/index.ts"),
   // output: バンドルされてあファイルを出力する場所、出力方法
-  output: { 
-    filename: 'index,js',
-    path: resolve(__dirname, 'dist'),
+  output: {
+    filename: "index,js",
+    path: resolve(__dirname, "dist"),
   },
   // resolve: モジュールの解決方法を指定する。拡張しを指定する。
   resolve: {
-    extension: ['.ts', '.js'],
+    extensions: [".ts", ".js"],
   },
   // module: モジュールとして扱いたいファイルを指定する。それおれloaderを記述する。
-  module: [
-    rule: [
+  module: {
+    rules: [
       {
         test: /\.ts/,
         use: {
-          loader: 'ts-loader',
+          loader: "ts-loader",
         },
       },
     ],
-  ]c
-}
+  },
+};
